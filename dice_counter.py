30f3c03681c6640dd8eca8338ee9475170470dba
Piotr Derenowski <56509461+piter104@users.noreply.github.com>
2020-11-13 18:19:02 +0100

    process image function added

--- dice_counter.py
+++ dice_counter.py
@@ -3,34 +3,45 @@ import skimage
 import numpy as np
 import random as rng
 import copy
+import matplotlib.pyplot as plt
 
 def normalize_image(image):
-    blur = skimage.filters.gaussian(image, sigma=20)
-    t = skimage.filters.threshold_otsu(blur)
+    blur = skimage.filters.gaussian(image, sigma=2)
+    x = round(np.std(image) + np.mean(image))
+    t = skimage.filters.threshold_otsu(x)
     mask = (blur > t)
     return  mask.astype(np.uint8)
     
 
 def remove_everything_above_std_and_mean(image):
-    x = round(np.std(image) + np.mean(image) )
-    return np.array((image > x) * 255, dtype=np.uint8)
-
-
+    image = skimage.filters.gaussian(image, sigma=20)
+    x = round(np.std(image) + np.mean(image))
+    return np.uint8(x)
 
 def draw_contours(image):
     copy_image = copy.deepcopy(image) 
     contours, hierarchy = cv.findContours(copy_image, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-    color = (rng.randint(200,256), rng.randint(220,250), rng.randint(0,20))
+    color = (rng.randint(120,130), rng.randint(120,130), rng.randint(120,130))
     cv.drawContours(copy_image, contours, -1, color, 5, cv.LINE_8, hierarchy, 2)
     return copy_image
+
+def process_image(image):
+    image = cv.resize(image, (800, 600))
+    image_grayscale = cv.cvtColor(255 - image, cv.COLOR_RGB2GRAY)
+    temp = remove_everything_above_std_and_mean(image_grayscale)
+    contoured_image1 = draw_contours(temp)
+    plt.figure(figsize=(25,25))
+    plt.subplot(1, 2, 1)
+    plt.imshow(image1)
+    plt.subplot(1, 2, 2)
+    plt.imshow(contoured_image1, cmap='gray')
+    return contoured_image1
 
 def count_contours(image):
     pass
 
 
 if __name__ == "__main__" :
-    import matplotlib.pyplot as plt
-
     img = cv.imread("images.png")
     cv.resize(img, (960, 720))
     contoured = normalize_image(img)
